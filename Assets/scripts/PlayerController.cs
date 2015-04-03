ba6eedade916c6746aded284b90c1de420f60b16
JoshHarris85 <joshua.harris10@okstate.edu>
2015-04-02 23:02:57 -0500

    Unity Roll A Ball Game Tutorial
    http://unity3d.com/learn/tutorials/projects/roll-a-ball

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+    public float speed;
+    private int count;
+	void Start () 
+    {
+        count = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        rigidbody.AddForce(movement * speed);
+
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "PickUp")
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+        }
+    }
+}
